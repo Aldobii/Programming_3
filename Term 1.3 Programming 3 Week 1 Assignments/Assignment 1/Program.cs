f19b9a1ad874b734e0c62dfbe1633130dcd07e45
Aldobii <42969068+Aldobii@users.noreply.github.com>
2019-02-04 15:23:17 +0100

    Creating the Thang

---
+++ Term 1.3 Programming 3 Week 1 Assignments/Assignment 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program Program1 = new Program();
+            Program1.Start();
+        }
+
+        void Start()
+        {
+
+        }
+    }
+}
